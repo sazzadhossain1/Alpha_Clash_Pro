edcfda92d58c3ce47ce36d237dbfeead629d6927
unknown <sazzadtoma143@gmail.com>
2024-09-12 16:24:48 +0600

    start javasctipt function

---
+++ index.js
@@ -0,0 +1,8 @@
+const play = () => {
+  const home_screen = document.getElementById("home_screen");
+  home_screen.classList.add("hidden");
+
+  const play_ground = document.getElementById("play_ground");
+  play_ground.classList.remove("hidden");
+};
+//  27 number module er 5 number ta ses and 6 number ta start -->
